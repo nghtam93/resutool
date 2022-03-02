fb832d212e229a1e1a8d768589c6d609d4172d61
doannguyen263 <doannguyen263@gmail.com>
2022-03-02 21:02:50 +0700

    update

--- js/main.js
+++ js/main.js
@@ -37,6 +37,80 @@ $(document).ready(function(){
         $(this).closest('.box__toggle').toggleClass('active').find('.box__toggle__content').slideToggle()
     })
 
+    $('.js-table-edit tr').hover(function(e) {
+        $('.js-table-edit tr').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    $('.table_list tbody input[type=checkbox]').click(function function_name(argument) {
+        // body...
+    })
+
+    // Table checkbox all
+    const DynamicCheckboxes = {
+
+        checkboxes: document.querySelectorAll('.table_list tbody input[type=checkbox]'),
+        selectAllTarget: document.querySelector('.js-checkbox-all input[type="checkbox"]'),
+
+        init(){
+            this.shiftToSelect();
+            this.selectAll();
+        },
+        shiftToSelect(){
+            const checkboxes = this.checkboxes;let lastChecked
+            function handleCheck(event) {
+                let check_checkbox = false
+                let between = false
+                if(event.shiftKey){
+                    checkboxes.forEach(checkbox => {
+                        if(checkbox === this || checkbox === lastChecked) between = !between
+                        if(between){
+                            checkbox.checked = true
+                            check_checkbox = true
+                        }
+                    });
+                }
+                lastChecked = this
+
+                // Add class checkbox
+                checkboxes.forEach(checkbox => {
+                    checkbox.closest('tr').classList.remove("row-acitve");
+                    if(checkbox.checked == true){
+                        check_checkbox = true
+                        checkbox.closest('tr').classList.add("row-acitve");
+                    }
+                });
+
+                $('.js-table-action-mb').removeClass('active')
+                if(check_checkbox == true){
+                    $('.js-table-action-mb').addClass('active')
+                }else{
+                    console.log(222222)
+                }
+            }
+            checkboxes.forEach(checkbox => {
+                checkbox.addEventListener("click",handleCheck,false)
+            });
+        },
+        selectAll(){
+            this.selectAllTarget.addEventListener("click",handleSelectAll.bind(this),false)
+            function handleSelectAll(){
+                let checked = false
+                if(this.selectAllTarget.checked) checked = true
+                this.checkboxes.forEach(checkbox => {
+                    checkbox.checked = checked
+                    checkbox.closest('tr').classList.remove("row-acitve");
+                    if(checkbox.checked == true){
+                        checkbox.closest('tr').classList.add("row-acitve");
+                    }
+                });
+            }
+        }
+      }
+    if($('.table_list').length){
+        DynamicCheckboxes.init()
+    }
+
 });
 
 
